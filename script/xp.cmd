b60284ca586bdb96bf215175325746d74485891e
Federico Kircheis <federico.kircheis@gmail.com>
2020-10-04 09:11:53 +0200

    Add settings for Windows XP

--- script/xp.cmd
+++ script/xp.cmd
@@ -6,6 +6,7 @@ echo HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WPAEvents [
 regini regini.ini
 del regini.ini
 
+:: stop unused services
 sc config "FastUserSwitchingCompatibility" start= disabled >NUL
 sc stop "FastUserSwitchingCompatibility" >NUL
 sc config "srsservice" start= disabled >NUL
@@ -15,6 +16,7 @@ sc stop "audiosrv" >NUL
 sc config "wzcsvc" start= disabled >NUL
 sc stop "wzcsvc" >NUL
 
+:: set theme
 :: install zune before (probably it's possible to just hack the registry keys together)
 sc config "themes" start= disabled >NUL
 sc stop "themes" >NUL
@@ -23,7 +25,7 @@ reg add "HKEY_CURRENT_USER\Control Panel\Colors" /v Background /t REG_SZ /d "0 0
 RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
 
 
-::  internet
+:: services that depend on internet connection (updated too)
 sc config "wuauserv" start= disabled >NUL
 sc stop "wuauserv" >NUL
 sc config "dhcp" start= disabled >NUL
@@ -36,31 +38,35 @@ sc config "wscsvc" start= disabled >NUL
 sc stop "wscsvc" >NUL
 
 powercfg /h off
-
-
 fsutil behavior set disable8dot3 1
 fsutil behavior set disablelastaccess 1
 
 cleanmgr /sagerun:1
 del /q/f/s %TEMP%\*
+del /q/f/s C:\Windows\temp\*
 
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v ScreenSaveActive /t REG_SZ /d 0 /f >NUL
 reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\" /v DisableLockWorkstation /t REG_DWORD /d 1 /f >NUL
 
-reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoDriveTypeAutoRun /t REG_DWORD /d 0xFF /f >NUL
 
 if exist %windir%\microsoft.net\framework\v4.0.30319\ngen.exe (
   %windir%\microsoft.net\framework\v4.0.30319\ngen.exe update /force /queue >NUL
   %windir%\microsoft.net\framework\v4.0.30319\ngen.exe executequeueditems >NUL
 )
 
-:: default settings
+:: security settings
+::	default settings
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\" /v authenticodeenabled /t REG_DWORD /d 0 /f >NUL
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\" /v DefaultLevel /t REG_DWORD /d 262144 /f >NUL
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\" /v PolicyScope /t REG_DWORD /d 0 /f >NUL
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\" /v TransparentEnabled /t REG_DWORD /d 1 /f >NUL
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\" /v ExecutableTypes /t REG_MULTI_SZ /d "ade\0adp\0bas\0bat\0chm\0cmd\0com\0cpl\0crt\0diagcab\0exe\0hlp\0hta\0inf\0ins\0isp\0mdb\0mde\0msc\0msi\0msp\0mst\0ocx\0pcd\0pif\0reg\0scr\0shs\0url\0vb\0vsix\0wsc\0application\0gadget\0vbs\0vbe\0js\0jse\0ws\0wsf\0wsh\0ps1\0ps1xml\0ps2\0ps2xml\0psc1\0psc2\0msh\0msh1\0msh2\0mshxml\0msh1xml\0msh2xml\0scf\0rgs"  /f >NUL
 
+::	sec policy, disable double extension for bat, exe and ps1 (more invasive than expected)
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\0\Paths\{82b63e32-c399-4570-aea4-861835929434}" /v Description /t REG_SZ /d "INIMA" /f >NUL
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\0\Paths\{82b63e32-c399-4570-aea4-861835929434}" /v SaferFlags /t REG_DWORD /d 0 /f >NUL
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\0\Paths\{82b63e32-c399-4570-aea4-861835929434}" /v Name /t REG_SZ /d "name"" /f >NUL
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\0\Paths\{82b63e32-c399-4570-aea4-861835929434}" /v ItemData /t REG_SZ /d "*.????.cmd"
 
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\0\Paths\{1e1be796-be75-45f3-8bc8-96298cf2cf77}" /v Description /t REG_SZ /d "INIMA" /f >NUL
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\0\Paths\{1e1be796-be75-45f3-8bc8-96298cf2cf77}" /v SaferFlags /t REG_DWORD /d 0 /f >NUL
@@ -76,3 +82,12 @@ reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\0\Paths\
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\0\Paths\{372587aa-01a8-4dd7-bf5e-ac854efa4100}" /v SaferFlags /t REG_DWORD /d 0 /f >NUL
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\0\Paths\{372587aa-01a8-4dd7-bf5e-ac854efa4100}" /v Name /t REG_SZ /d "name"" /f >NUL
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Safer\CodeIdentifiers\0\Paths\{372587aa-01a8-4dd7-bf5e-ac854efa4100}" /v ItemData /t REG_SZ /d "*.????.ps1"
+
+::	sec policy, hidden files, extensions, autoruns
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REF_DWORD /d 1 >NUL
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v Hidden /t REF_DWORD /d 1 >NUL
+
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REF_DWORD /d 0 >NUL
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REF_DWORD /d 0 >NUL
+
+reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v NoDriveTypeAutoRun /t REG_DWORD /d 0xFF /f >NUL
